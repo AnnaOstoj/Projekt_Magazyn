c6f1a8245021eeb7f71b31faa027b841a872fc39
Anna Ostojska <anna.ostojskaa@gmail.com>
2020-07-02 18:09:34 +0200

    Load stock

--- program_magazynowy.py
+++ program_magazynowy.py
@@ -1,26 +1,34 @@
 import operations as op
+import sys
 
-items = [{"name" : "Krzesło",  "quantity" : 50, "unit" : "pc", "unit_price" : 200},\
-        {"name" : "Stół",  "quantity" : 20, "unit" : "pc", "unit_price" : 500},\
-        {"name" : "Stołek",  "quantity" : 99, "unit" : "pc", "unit_price" : 90}]
-
+operation_dict = {"exit": op.close_application, "show":op.get_items, "add":op.add_item, "sell":op.sell_items,\
+         "show_revenue": op.show_revenue, "save":op.export_items_to_csv, "load":op.load_items_from_csv}
+items = []
 sold_items = []
-operation_dict = {"exit": op.close_application, "show":op.get_items, "add":op.add_item, "sell":op.sell_items, "show_revenue": op.show_revenue}
-
 while True:
-    operation_type = input("What would you like to do? Type one of: exit, show, add, sell, show_revenue: ")
+    operation_type = input("What would you like to do? Type one of: exit, show, add, sell, show_revenue, save, load: ")
+    
     if operation_type == "exit":
         op.close_application()
         break
+
     elif operation_type == "sell":
         op.sell_items(items, sold_items)
+
     elif operation_type == "show_revenue":
-        costs = op.get_costs(items)
-        income = op.get_income(sold_items)
-        print(op.show_revenue(costs, income))
+        op.show_revenue(items, sold_items)
+
     elif operation_type == "show":
-        print(op.get_items(items))
+        op.get_items(items)
+
     elif operation_type == "add":
         op.add_item(items)
         op.get_items(items)
-        
+
+    elif operation_type == "save":
+        op.export_items_to_csv(items, sys.argv[1])
+        op.export_sales_to_csv(sold_items, sys.argv[2])
+
+    elif operation_type == "load":
+        op.load_items_from_csv(items, sys.argv[1])
+        op.get_items(items)
