4834cc9ecb067581d84d2491e1e1783a9f37385a
Anna Ostojska <anna.ostojskaa@gmail.com>
2020-07-02 09:16:48 +0200

    Add function

---
+++ program_magazynowy.py
@@ -0,0 +1,16 @@
+import operations as op
+
+items = [{"name" : "Krzesło",  "quantity" : 50, "unit" : "pc", "unit_price" : 200},\
+        {"name" : "Stół",  "quantity" : 20, "unit" : "pc", "unit_price" : 500},\
+        {"name" : "Stołek",  "quantity" : 99, "unit" : "pc", "unit_price" : 90}]
+
+
+
+
+"""
+typy operacji : exit, show
+operation_type = input("What would you like to do?")
+
+op.close_application()
+op.show(items)
+"""
